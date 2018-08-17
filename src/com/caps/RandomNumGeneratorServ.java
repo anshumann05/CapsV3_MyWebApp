0156f19a74c5bbf1af1477d4b071b3805a9fb431
creatorHead <droidbit1@gmail.com>
2018-08-17 09:45:01 +0530

    Login Page added

--- src/com/caps/RandomNumGeneratorServ.java
+++ src/com/caps/RandomNumGeneratorServ.java
@@ -2,39 +2,20 @@ package com.caps;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/date")
-public class MyFirstServlet extends HttpServlet {
+public class RandomNumGeneratorServ extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String fname = req.getParameter("fname");
-		String lname = req.getParameter("lname");
 		
-		//int i = 10/0;
+		String randomNum = UUID.randomUUID().toString();
 		
 		PrintWriter out = resp.getWriter();
-		out.println("<h1>The Current Time is: "+ new Date() +"</h1>");
-		out.println("<h1>Name: "+fname+" "+lname+"</h1>");
-		
-		resp.setContentType("text/html");
+		out.println("<h1>The Randon Number is: "+randomNum + "</h1>");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
